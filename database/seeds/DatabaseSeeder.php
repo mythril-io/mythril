71ec619f3a3e771f20ad4e8016bf118c9e7aac63
James Moore <jamesm0025@gmail.com>
2019-04-25 13:11:27 -0400

    users table seeder not working

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Create at least one test user for people working on app
+        $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // factory(App\User::class, 1)->create(['email' => 'mythril@test.com']);
+        factory(App\User::class, 50)->create();
+    }
+}
