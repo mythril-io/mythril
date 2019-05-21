b04934148fafffa8a4fc4ba625d2eb28c08541dd
unknown <inderbir.s97@gmail.com>
2019-05-21 00:24:44 -0400

    Bug Fixes

--- resources/assets/js/components/pages/forums/discussions/components/DiscussionItem.vue
+++ resources/assets/js/components/pages/forums/discussions/components/DiscussionItem.vue
@@ -25,8 +25,12 @@
 
                     </p>
                     <p class="heading">
-                        <router-link v-if="discussion.last_post" :to="{name: 'User', params: { id: discussion.last_post.user.id }}" class="underline-link">{{ discussion.last_post.user.username }}</router-link>
-                        <router-link v-else :to="{name: 'User', params: { id: discussion.user.id }}" class="underline-link">{{ discussion.user.username }}</router-link>
+                        <span v-if="discussion.last_post">
+                            <router-link :to="{name: 'User', params: { id: discussion.last_post.user.id }}" class="underline-link">{{ discussion.last_post.user.username }}sdfd</router-link>
+                        </span>
+                        <span v-else>
+                            <router-link :to="{name: 'User', params: { id: discussion.user.id }}" class="underline-link">{{ discussion.user.username }}</router-link>
+                        </span>
                         - {{ discussion.last_posted_at | ago($store.user)  }}
                     </p>
 
